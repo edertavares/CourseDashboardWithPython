a987bbdf3b69a6e965f1b8332a6b9e489d1d3c30
Éder Lucio Tavares <edert.tavares@gmail.com>
2025-08-11 17:12:56 -0300

    Fixes in the code related to the data type.

--- section two/usingInput.py
+++ section two/usingInput.py
@@ -1,16 +1,26 @@
 #aprendendo a usar o imput
 import os
 
+os.system("cls" if os.name == "nt" else "clear")#limpando a tela ao iniciar o programa
 name = input("Qual o nome do filme?\n")
-yearLauche = input("Qual o ano de lançamento?\n")
-noteMove = input("Qual a nota do filme?\n")
+yearLauche = int(input("Qual o ano de lançamento?\n"))
+noteMove = float(input("Qual a nota do filme?\n"))
 planIncluded = input("O plano está incluído? (True/False)\n")
 
+#limpando a tela
 os.system("cls" if os.name == "nt" else "clear")
 print()  #linha em branco
 
 #exibindo os dados na tela
 print("O nome do filme é: " + name)
-print("O ano de lançamento é: " + yearLauche)
-print("A nota do filme é: " + noteMove)
+print("O ano de lançamento é: " + str(yearLauche))
+print("A nota do filme é: " + str(noteMove))
 print("O plano está incluído: " + planIncluded)
+
+print()  #linha em branco
+print("Exibindo o tipo de dados: \n")
+#exibindo os tipos dos dados na tela
+print((type(name)))
+print((type(yearLauche)))
+print((type(noteMove)))
+print((type(planIncluded)))
